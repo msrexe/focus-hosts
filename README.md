b3ff178fb9c314ebc0ac4efbc9709e7009bc84e5
Melih Sivri <mlheymen.ms@gmail.com>
2024-06-02 13:36:13 +0300

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# Focus script
+
+This script is used to focus on a specific task by blocking access to certain websites for a certain amount of time.
+
+## Usage
+
+- Fill the `block-list.txt` file with the websites you want to block.
+
+- Run the script with the duration you want to block the websites for.
+
+```bash
+chmod +x focus.sh
+
+sudo ./focus.sh --duration=30m or --duration=1h
+```
---
+++ focus.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+usage() {
+  echo "Usage: sudo ./focus.sh --duration=30m or sudo ./focus.sh --duration=3h"
+  exit 1
+}
+
+if [ "$#" -ne 1 ]; then
+  usage
+fi
+
+case "$1" in
+  --duration=*)
+    DURATION="${1#*=}"
+    ;;
+  *)
+    usage
+    ;;
+esac
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+if [ ! -f "block-list.txt" ]; then
+  echo "block-list.txt not found"
+  exit
+fi
+if [ ! -f "/etc/hosts" ]; then
+  echo "/etc/hosts not found"
+  exit
+fi
+
+sed -i '' '/# Block-list start/,/# Block-list end/d' /etc/hosts
+
+echo "# Block-list start" >> /etc/hosts
+while IFS= read -r line; do
+  echo "127.0.0.1 $line" >> /etc/hosts
+  echo "127.0.0.1 www.$line" >> /etc/hosts
+done < block-list.txt
+echo "# Block-list end" >> /etc/hosts
+
+echo "Focus session started for $DURATION."
+
+case "$DURATION" in
+  *m)
+    SECONDS=$((${DURATION%m} * 60))
+    ;;
+  *h)
+    SECONDS=$((${DURATION%h} * 3600))
+    ;;
+  *)
+    usage
+    ;;
+esac
+
+sleep $SECONDS
+
+sed -i '' '/# Block-list start/,/# Block-list end/d' /etc/hosts
+
+echo "Focus session ended. /etc/hosts restored to original state."
